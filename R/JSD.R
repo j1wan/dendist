182a2f7d7fe128180fe2ea9d7882f59d5d205ada
j1wan <jiwanjeong@gmail.com>
2016-08-28 21:22:37 +0900

    first commit

---
+++ R/JSD.R
@@ -0,0 +1,26 @@
+#' Jensen-Shannon Distance/Divergence
+#'
+#' Calcuate Jensen-Shannon distance/divergence
+#' @aliases JSDist
+#' @param P y-values of a distribution
+#' @param Q y-values of another distribution
+#' @param dx delta x
+#' @seealso \code{\link{KLDiv}}
+#' @export
+JSDiv <- function(P, Q, dx) {
+  M <- (P + Q) / 2
+  D <- (KLDiv(P, M, dx) + KLDiv(Q, M, dx)) / 2
+  if (is.na(D)) {
+    return(NA)
+  } else if (D < 0) { # floating number fix
+    return(0)
+  } else {
+    return(D)
+  }
+}
+
+#' @export
+JSDist <- function(P, Q, dx) {
+  return(sqrt(JSDiv(P, Q, dx)))
+}
+
---
+++ README.md
@@ -0,0 +1,7 @@
+# dendist
+
+An R package to calculate ~dist~ance between probability ~den~sity functions. Now it supports
+- Kullback-Leibler divergence (KLDiv)
+- Jensen-Shannon divergence (JSDiv)
+- Jensen-Shannon distance (JSDist)
+
